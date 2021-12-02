0e8db218f40848e36b43b99e490f5865a6516936
Priyesh Chandra <priyesh.chandra@argildx.net>
2021-12-02 15:36:45 +0530

    "Deleted redundant code in portlet class"

--- modules/Employee-web/src/main/java/com/liferay/assignment/employee/portlet/EmployeeWebPortlet.java
+++ modules/Employee-web/src/main/java/com/liferay/assignment/employee/portlet/EmployeeWebPortlet.java
@@ -1,27 +1,11 @@
 package com.liferay.assignment.employee.portlet;
 
-import com.liferay.assignment.empApi.api.EmployeeApi;
 import com.liferay.assignment.employee.constants.EmployeeWebPortletKeys;
-import com.liferay.employee.assignment.model.Employee;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.ServiceContextFactory;
-import com.liferay.portal.kernel.util.ParamUtil;
 
-import java.io.IOException;
-import java.util.List;
-
-import javax.portlet.ActionRequest;
-import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
-import javax.portlet.PortletException;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Argil DX
@@ -34,31 +18,5 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.security-role-ref=power-user,user" }, service = Portlet.class)
 public class EmployeeWebPortlet extends MVCPortlet {
 
-	@Override
-	public void render(RenderRequest renderRequest, RenderResponse renderResponse)
-			throws IOException, PortletException { // TODO Auto-generated
-		super.render(renderRequest, renderResponse);
-	}
-
 	
-
-	
-
-	@SuppressWarnings("deprecation")
-	public void viewEntry(ActionRequest request, ActionResponse response) throws PortalException, SystemException {
-
-		List<Employee> employeeList = _employeeAPI.getAllEmployees();
-		request.setAttribute("employeeList", employeeList);
-		System.out.println("Portlet respose" + employeeList);
-		response.setRenderParameter("mvcPath", "/viewEmployees.jsp");
-
-		// response.getRenderParameters().setValue("mvcPath", "/viewEmployee.jsp");
-		// System.out.println("after :");
-	}
-
-	// @Reference
-	// private EmployeeLocalService _employeeLocalService;
-
-	@Reference
-	private EmployeeApi _employeeAPI;
 }
